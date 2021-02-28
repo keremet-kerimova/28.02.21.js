2dd2b9bc98e786866b217051dbd976801b91a1ab
Your Name <you@example.com>
2021-02-28 20:34:10 +0600

    two componens with code

---
+++ src/components/Chunga.js
@@ -0,0 +1,4 @@
+function Chunga() {
+    return <h1>Chunga</h1>;
+}
+export default Chunga;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+
+function HelloWorld() {
+    return <h1>HelloWorld</h1>;
+}
+export default HelloWorld;
+
+
+
+ 
